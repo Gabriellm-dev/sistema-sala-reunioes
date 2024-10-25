889f8ead3850508855f8343c01ec1f637edd154a
gabriellm-dev <gabriellm.dev@gmail.com>
2024-10-25 15:53:12 -0300

    Implementado classe Sala Repository

---
+++ src/main/java/com/br/saladereunioes/repository/SalaRepository.java
@@ -0,0 +1,77 @@
+package com.br.saladereunioes.repository;
+
+import org.bson.Document;
+import com.br.saladereunioes.database.MongoConnection;
+import com.br.saladereunioes.model.Sala;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class SalaRepository {
+    
+    private MongoDatabase database;
+
+    public SalaRepository(MongoDatabase database) {
+        this.database = database != null ? database : MongoConnection.getDatabase("salareuniaodb");
+    }
+
+    private MongoCollection<Document> getCollection() {
+        return database.getCollection("salas");
+    }
+
+    public void addSala(Sala sala){
+        Document doc = new Document("nome", sala.getNome())
+            .append("capacidade", sala.getCapacidade())
+            .append("recursos", sala.getRecursos())
+            .append("status", sala.getStatus());
+        getCollection().insertOne(doc);
+    }
+
+    public void updateSala(Sala sala){
+        Document doc = new Document("nome", sala.getNome())
+            .append("capacidade", sala.getCapacidade())
+            .append("recursos", sala.getRecursos())
+            .append("status", sala.getStatus());
+        getCollection().updateOne(new Document("_id", sala.getId()), new Document("$set", doc));
+    }
+
+    public void deleteSala(Object id){
+        getCollection().deleteOne(new Document("_id", id));
+    }
+
+    public Sala getSala(Object id){
+        Document doc = getCollection().find(new Document("_id", id)).first();
+        if (doc == null) {
+            return null;
+        }
+        return new Sala(
+            doc.getString("nome"),
+            doc.getInteger("capacidade"),
+            doc.getList("recursos", String.class).toArray(new String[0]),
+            doc.getString("status")
+        );
+    }
+
+    public void deleteAll(){
+        getCollection().deleteMany(new Document());
+    }
+
+    public void updateStatus(Object id, String status, Integer capacidade, String[] recursos, String nome) {
+        Document updates = new Document("status", status);
+    
+        if (capacidade != null) {
+            updates.append("capacidade", capacidade);
+        }
+        if (recursos != null) {
+            updates.append("recursos", recursos);
+        }
+        if (nome != null) {
+            updates.append("nome", nome);
+        }
+    
+        getCollection().updateOne(new Document("_id", id), new Document("$set", updates));
+    }
+    
+    public void updateStatus(Sala sala, Integer capacidade, String[] recursos, String nome) {
+        updateStatus(sala.getId(), sala.getStatus(), capacidade, recursos, nome);
+    }
+}
